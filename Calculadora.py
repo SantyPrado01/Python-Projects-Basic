d3da43150b9d07e81728bffb5b58975d4f00a865
Santy <santiago01prado@gmail.com>
2022-12-29 18:04:27 -0300

    2 Proyectos

---
+++ Calculadora.py
@@ -0,0 +1,24 @@
+opc = int(input("Calculadora Simple \n Opcion 1 --> Suma \n Opcion 2 --> Resta \n Opcion 3 --> Multiplicacion \n Opcion 4 -->Division \n Elige la opcion: "))
+
+if opc == 1 :
+    num_1 = int(input("Escribe el primer numero: "))
+    num_2 = int(input("Escribe el segundo numero: "))
+    resultado = num_1 + num_2
+    print("El resultado de la suma es ", resultado)
+elif opc == 2 :
+    num_1 = int(input("Escribe el primer numero: "))
+    num_2 = int(input("Escribe el segundo numero: "))
+    resultado = num_1 - num_2
+    print("El resultado de la resta es ", resultado)
+elif opc == 3 :
+    num_1 = int(input("Escribe el primer numero: "))
+    num_2 = int(input("Escribe el segundo numero: "))
+    resultado = num_1 * num_2
+    print("El resultado de la multiplicacion es ", resultado)
+elif opc == 4 :
+    num_1 = int(input("Escribe el primer numero: "))
+    num_2 = int(input("Escribe el segundo numero: "))
+    resultado = num_1 / num_2
+    print("El resultado de la division es ", resultado)
+else:
+    print("La opcion seleccionada no es disponible.")
---
+++ ContadorDePalabras.py
@@ -0,0 +1,5 @@
+text = input("En que estas pensando?")
+
+pensamiento = len(text.split())
+
+print("Tu pensamiento esta formado por ",pensamiento, " palabras.")
