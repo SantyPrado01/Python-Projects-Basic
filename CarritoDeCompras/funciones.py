5c37be626e3d4d13715ebf6c4abaaccc8997985a
SantyPrado01 <santiago01prado@gmail.com>
2023-06-23 14:34:17 -0300

    Correcciones Modificacion de Carrito

--- CarritoDeCompras/funciones.py
+++ CarritoDeCompras/funciones.py
@@ -66,10 +66,25 @@ class CarritoCompras:
             for producto, cantidad in self.productos:
 
                 print(f'{cantidad} unidad(es) de {producto.nombre}')
+                
             print(f'El monto total a pagar es: ${self.monto_total}')
             print('=='*45)
             print(f'Factura B Cliente: {nombre} Fecha: {date.today()}') 
-            return(True)   
+            return(True) 
+        
+    def mostrar_carrito_dos(self):
+        
+        print('=='*45)
+            
+        print('Productos en el carrito:')
+            
+        for producto, cantidad in self.productos:
+
+            print(f'{cantidad} unidad(es) de {producto.nombre}')
+                
+        print(f'El monto total a pagar es: ${self.monto_total}')
+        print('=='*45)
+  
                 
 carrito = CarritoCompras()
      
@@ -176,46 +191,55 @@ def carrito_agregar():
             break
 
 def modificar_carrito():
-    
     while True:
-        try:    
+        try:
             modificar = int(input('Desea modificar el carrito? (Si --> 1, No --> 2): '))
             if modificar == 1:
-                carrito.mostrar_carrito()
-        
-                producto_modifcar = int(input('Ingresa el codigo del producto a modificar: '))
-        
+                carrito.mostrar_carrito_dos()
+
+                producto_modificar = int(input('Ingresa el código del producto a modificar: '))
+
                 producto_encontrado = None
-                for Producto in productos:
-                    if Producto.codigo == producto_modifcar:
-                        producto_encontrado = Producto
+                index_producto = None
+                for i, (producto, cantidad) in enumerate(carrito.productos):
+                    if producto.codigo == producto_modificar:
+                        producto_encontrado = producto
+                        index_producto = i
                         break
-                    if producto_encontrado:
-                        producto = producto_encontrado
-                        cantidad_nueva = int(input(f'Ingrese la nueva cantidad para el producto {producto.nombre}: '))
-            
-                    if producto.verificar_stock(cantidad_nueva):
-                
-                        cantidad_actual = carrito.obtener_cantidad_producto(producto)
-                        diferencia = cantidad_nueva - cantidad_actual
-             
-                        producto.modificar_stock(diferencia)
-                
-                        carrito.modificar_cantidad_producto(producto, cantidad_nueva)
-            
-                        print(f'Producto {producto.nombre} modificado correctamente en el carrito.')
-                        print('=='*45)
-                 
+
+                if producto_encontrado:
+                    cantidad_nueva = int(input(f'Ingrese la nueva cantidad para el producto {producto_encontrado.nombre}: '))
+
+                    if cantidad_nueva < cantidad:
+                        cantidad_devolucion = cantidad - cantidad_nueva
+                        producto_encontrado.modificar_stock(cantidad_devolucion)
+                        carrito.productos[index_producto] = (producto_encontrado, cantidad_nueva)
+                        carrito.monto_total -= cantidad_devolucion * producto_encontrado.precio
+                        print(f'Se devolvieron {cantidad_devolucion} unidad(es) de {producto_encontrado.nombre} al stock.')
+                        print('==' * 45)
+                    elif cantidad_nueva > cantidad:
+                        cantidad_a_agregar = cantidad_nueva - cantidad
+                        if producto_encontrado.verificar_stock(cantidad_a_agregar):
+                            producto_encontrado.modificar_stock(cantidad_a_agregar)
+                            carrito.productos[index_producto] = (producto_encontrado, cantidad_nueva)
+                            carrito.monto_total += cantidad_a_agregar * producto_encontrado.precio
+                            print(f'Se agregaron {cantidad_a_agregar} unidad(es) de {producto_encontrado.nombre} al carrito.')
+                            print('==' * 45)
+                        else:
+                            print(f'No hay suficiente stock de {producto_encontrado.nombre} para la cantidad deseada.')
+                            print('==' * 45)
                     else:
-                        print(f'No hay suficiente stock de {producto.nombre} para la cantidad deseada.')
-                        print('=='*45)
+                        print(f'No se realizaron modificaciones en la cantidad de {producto_encontrado.nombre}.')
+                        print('==' * 45)
                 else:
                     print('Producto no encontrado')
-                    print('=='*45)
+                    print('==' * 45)
             else:
                 print('No se realizarán modificaciones en el carrito.')
-                print('=='*45)
+                print('==' * 45)
         except ValueError:
-            print('No ingresaste una opcion valida, intenta nuevamente')
+            print('No ingresaste una opción válida, intenta nuevamente')
         else:
             break
+
+
