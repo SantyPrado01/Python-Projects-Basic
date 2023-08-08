e451b2c5e9d8714a6c071364e83fa50d44adfe3e
Santy <santiago01prado@gmail.com>
2023-08-08 12:04:40 -0300

    Cambios en la calculadora

--- Calculadora_sin_Interfaz/Calculadora.py
+++ Calculadora_sin_Interfaz/Calculadora.py
@@ -1,28 +1,64 @@
-"""#Primera Solucion
-#opc = int(input("Calculadora Simple \n Opcion 1 --> Suma \n Opcion 2 --> Resta \n Opcion 3 --> Multiplicacion \n Opcion 4 -->Division \n Elige la opcion: "))
+def esNumero(a):
+    try:
+        a = int(a)
+        return(a)
+    except:
+        print('No es un numero')
 
-#if opc == 1 :
+def ingresar_numero():
+    global num_1, num_2
     num_1 = int(input("Escribe el primer numero: "))
     num_2 = int(input("Escribe el segundo numero: "))
+    if esNumero(num_1) and esNumero(num_2): 
+        return(num_1, num_2)
+
+def sumar(num_1,num_2):
     resultado = num_1 + num_2
-    print("El resultado de la suma es ", resultado)
+    print(f'El resultado de la operacion es: {resultado}') 
+
+def restar(a,b):
+    resultado = a-b
+    print(f'El resultado de la operacion es: {resultado}') 
+    
+def multiplicacion(a,b):
+    resultado = a*b
+    print(f'El resultado de la operacion es: {resultado}') 
+
+def division(a,b):
+    resultado = a/b
+    print(f'El resultado de la operacion es: {resultado}') 
+
+def potencia(a,b):
+    resultado = a**b
+    print(f'El resultado de la operacion es: {resultado}') 
+
+def radicacion(a,b):
+    resultado = a**(1/b)
+    print(f'El resultado de la operacion es: {resultado}') 
+        
+#Primera Solucion
+opc = int(input("Calculadora Simple \n Opcion 1 --> Suma \n Opcion 2 --> Resta \n Opcion 3 --> Multiplicacion \n Opcion 4 -->Division \n Opcion 5 -->Potencia \n Opcion 6 -->Radicacion \nElige la opcion: "))
+
+if opc == 1 :
+    ingresar_numero()
+    sumar(num_1, num_2)
 elif opc == 2 :
-    num_1 = int(input("Escribe el primer numero: "))
-    num_2 = int(input("Escribe el segundo numero: "))
-    resultado = num_1 - num_2
-    print("El resultado de la resta es ", resultado)
+    ingresar_numero()
+    restar(num_1, num_2)
 elif opc == 3 :
-    num_1 = int(input("Escribe el primer numero: "))
-    num_2 = int(input("Escribe el segundo numero: "))
-    resultado = num_1 * num_2
-    print("El resultado de la multiplicacion es ", resultado)
+    ingresar_numero()
+    multiplicacion(num_1, num_2)
 elif opc == 4 :
-    num_1 = int(input("Escribe el primer numero: "))
-    num_2 = int(input("Escribe el segundo numero: "))
-    resultado = num_1 / num_2
-    print("El resultado de la division es ", resultado)
+    ingresar_numero()
+    division(num_1, num_2)
+elif opc == 5:
+    ingresar_numero()
+    potencia(num_1, num_2)
+elif opc == 6:
+    ingresar_numero()
+    radicacion(num_1, num_2)
 else:
-    print("La opcion seleccionada no es disponible.")"""
+    print("La opcion seleccionada no es disponible.")
 
     
 """#Segunda Solucion    
@@ -32,54 +68,5 @@ ing_num = input("Ingresa la operacion a realizar: ")
 print('El Resultado es: ', eval(ing_num))
     """
 
-def esNumero(a):
-    try:
-        a = int(a)
-        return(a)
-    except:
-        print('No es un numero')
 
 
-def sumar(a,b):
-    if esNumero(a) and esNumero(b): 
-        resultado = a + b
-        return resultado
-    else:
-        print('No ingresaste un numero')
-
-def restar(a,b):
-    if esNumero(a) and esNumero(b):
-        resultado = a-b
-        return(resultado)
-    else:
-        print('No ingresaste un numero')
-    
-def multiplicacion(a,b):
-    if esNumero(a) and esNumero(b):
-        resultado = a*b
-        return(resultado)
-    else:
-        print('No ingresaste un numero')
-
-def division(a,b):
-    if esNumero(a) and esNumero(b):
-        resultado = a/b
-        return(resultado)
-    else:
-        print('No ingresaste un numero')
-
-def potencia(a,b):
-    if esNumero(a) and esNumero(b):
-        resultado = a**b
-        return(resultado)
-    else:
-        print('No ingresaste un numero')
-
-def radicacion(a,b):
-    if esNumero(a) and esNumero(b):
-        resultado = a**(1/b)
-        print(resultado)
-    else:
-        print('No ingresaste un numero')
-
-radicacion('q', 2)
